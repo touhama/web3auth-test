dda9c7da9c0d6374ab9043acd4b9e01c6dab096c
touhama <touhama.cdv@gmail.com>
2025-09-09 18:52:32 +0900

    fix: Buffer polyfill for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
+  },
+  define: {
+    'process.env': {}
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer'
+    }
+  }
 })
