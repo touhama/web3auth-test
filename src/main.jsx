61ad28f964a1d751ee1aa529eaa345697ffb0f37
touhama <touhama.cdv@gmail.com>
2025-09-09 20:36:02 +0900

    fix: Buffer/process polyfill for Vercel browser

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Buffer } from "buffer";
-
+import process from "process";
 window.Buffer = Buffer;
+window.process = process;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
